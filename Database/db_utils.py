1f023333d9846a5b1ce38e5977899f2bf2e7da24
tylerstone835 <144369580+tylerstone835@users.noreply.github.com>
2025-08-02 17:16:53 -0600

    Initial Commit

---
+++ Database/db_utils.py
@@ -0,0 +1,44 @@
+import os
+
+import sqlite3
+
+database_fp = os.environ.get('STOCK_DATABASE')
+
+
+def create_db_table_from_map(
+    table_name: str,
+    map: dict,
+    database_fp: str = database_fp,
+) -> None:
+    """
+    Parses information from map to construct/execute a create table statement
+    in designated SQLite database.
+
+    :param table_name: Name for created table.
+    :param map: dict object containing column names (keys) and dtype/constraint values.
+    :param database_fp: filepath to target database.
+    """
+
+    column_definitions = ',\n\t'.join([f'{column} {map[column]['dtype']} {map[column]['constraint']}'
+                                       for column in map]).replace(' PRIMARY KEY', '')
+
+    primary_key = ', '.join([column for column in map if map[column]['constraint'] == 'PRIMARY KEY'])
+
+    if not primary_key:
+        raise ValueError('Primary key(s) not found in table map.')
+
+    create_table_statement = f"""
+        CREATE TABLE {table_name}
+        (
+        {column_definitions},
+        PRIMARY KEY ({primary_key})
+        );
+    """
+
+    with sqlite3.connect(database_fp) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute('BEGIN TRANSACTION;')
+        cursor.execute(f'DROP TABLE IF EXISTS {table_name};')
+        cursor.execute(create_table_statement)
+        cursor.execute('COMMIT;')

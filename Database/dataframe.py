678496faf12ef9e27731796f35a0d5cb15d3a573
tylerstone835 <144369580+tylerstone835@users.noreply.github.com>
2025-08-02 12:34:02 -0600

    Reworked natural_join to take any number of dfs as arguments.

--- Database/dataframe.py
+++ Database/dataframe.py
@@ -1,27 +1,62 @@
+from datetime import datetime
+
 import pandas as pd
+
+from table_maps import daily_table_map
 
 
 def natural_join(
-        left: pd.DataFrame,
-        right: pd.DataFrame,
+        *dfs: pd.DataFrame,
         how: str = 'inner',
 ) -> pd.DataFrame | None:
     """
     Merges two pd.DataFrame objects based on overlapping column labels.
-        :param left: Left pd.DataFrame in the join.
-        :param right: Right pd.DataFrame in the join.
+        :param *dfs: One or many dataframe objects to naturally join.
         :param how: Join type to be performed (e.g., inner, left, right, outer, cross)
         :return: Joined pd.DataFrame object.
     """
-    columns_list = list(left.columns) + list(right.columns)
+    master_df = pd.DataFrame()
 
-    dupe_labels_df = (pd.Series(columns_list)
-                      .value_counts()
-                      .reset_index()
-                      )
-    dupe_labels_list = dupe_labels_df.loc[dupe_labels_df['count'] > 1]['index'].to_list()
+    for df in dfs:
+        if master_df.empty:
+            master_df = df
+            continue
 
-    if not dupe_labels_list:
-        return
+        columns_list = list(master_df.columns) + list(df.columns)
 
-    return pd.merge(left=left, right=right, how=how, on=dupe_labels_list)
+        dupe_labels_df = (
+            pd.Series(columns_list)
+            .value_counts()
+            .reset_index()
+        )
+
+        dupe_labels_list = dupe_labels_df.loc[dupe_labels_df['count'] > 1]['index'].to_list()
+
+        if dupe_labels_list:
+            master_df = master_df.merge(df, how=how, on=dupe_labels_list)
+
+    return master_df
+
+
+def build_df_from_map(
+    table_map: dict,
+    ticker: str,
+) -> pd.DataFrame:
+    """
+    Creates pd.DataFrame for designated symbol based on the table_map structure.
+
+    :param table_map: dict object containing the callables and params necessary to make table in map.
+    :ticker: Ticker symbol for a publicly traded stock.
+    """
+
+    df_list = []
+    for column in table_map:
+        if table_map[column]['callable'] is None or table_map[column]['params'] is None:
+            continue
+
+        callable = table_map[column]['callable']
+        params = table_map[column]['params']
+
+        df_list.append(callable(ticker=ticker, **params ))
+
+    return natural_join(*df_list, how='inner')

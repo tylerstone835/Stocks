efb324801aa3656e74b366964288ae36ba5bfa84
tylerstone835 <144369580+tylerstone835@users.noreply.github.com>
2025-08-02 18:45:10 -0600

    Removed default database.

--- Database/create_database.py
+++ Database/create_database.py
@@ -1,20 +1,27 @@
-from polygon_api import get_active_tickers
+import os
+
 from dataframe import build_df_from_map
-from table_maps import daily_table_map
 from db_utils import create_db_table_from_map
+from polygon_api import get_active_tickers
+from table_maps import daily_table_map
+
 
 def main():
 
+    database_filepath = os.environ.get('STOCK_DATABASE')
+
     # Create database tables
     create_db_table_from_map(
+        database_fp=database_filepath,
         map=daily_table_map,
         table_name='daily'
     )
 
-    # ticker_list = get_active_tickers()
-    #
-    # for ticker in ticker_list:
-    #     daily_df = build_df_from_map(ticker=ticker, map=daily_table_map)
+    ticker_list = get_active_tickers()
+
+    for ticker in ticker_list:
+        daily_df = build_df_from_map(ticker=ticker, map=daily_table_map)
+
 
 if __name__ == '__main__':
     main()

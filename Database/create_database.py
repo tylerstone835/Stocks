b969c6fbbed4ea27c8b1dd2cd2d14678cd3e8e40
tylerstone835 <144369580+tylerstone835@users.noreply.github.com>
2025-08-05 01:06:59 -0600

    Basic daily and weekly table pipelines finished.

--- Database/create_database.py
+++ Database/create_database.py
@@ -1,26 +1,74 @@
 import os
 
-from dataframe import build_df_from_map
-from db_utils import create_db_table_from_map
-from polygon_api import get_active_tickers
-from table_maps import daily_table_map
+from dataframe import *
+from db_utils import create_table_from_map, insert_data
+from polygon_api import *
+from table_maps import price_fact_map
 
 
 def main():
+    DB_FILEPATH = os.environ.get('STOCK_DATABASE')
 
-    database_filepath = os.environ.get('STOCK_DATABASE')
-
-    # Create database tables
-    create_db_table_from_map(
-        database_fp=database_filepath,
-        map=daily_table_map,
+    # Create daily SQL table.
+    create_table_from_map(
+        db_filepath=DB_FILEPATH,
+        map=price_fact_map,
         table_name='daily'
+    )
+
+    # Create weekly SQL table.
+    create_table_from_map(
+        db_filepath=DB_FILEPATH,
+        map=price_fact_map,
+        table_name='weekly'
     )
 
     ticker_list = get_active_tickers()
 
-    for ticker in ticker_list:
-        daily_df = build_df_from_map(ticker=ticker, map=daily_table_map)
+    for ticker in ticker_list[:5]:
+
+        # Construct daily pd.DataFrame for ticker and insert into table.
+        daily_df = natural_join(
+                       get_price_action(ticker=ticker),
+                       get_sma(ticker=ticker, window=50),
+                       get_ema(ticker=ticker, window=5),
+                       get_ema(ticker=ticker, window=10),
+                       get_ema(ticker=ticker, window=20),
+                       get_macd(ticker=ticker)
+                   )
+
+        calculate_keltner_channels(daily_df)
+        calculate_atr(daily_df)
+        calculate_impulse(daily_df)
+        timestamp_to_date(daily_df)
+
+        insert_data(
+            df=daily_df,
+            table_name='daily',
+            db_filepath=DB_FILEPATH
+        )
+
+        # Construct weekly pd.DataFrame for ticker and insert into table.
+        weekly_df = natural_join(
+                        get_price_action(ticker=ticker, timespan='week'),
+                        get_sma(ticker=ticker, window=50, timespan='week'),
+                        get_ema(ticker=ticker, window=5, timespan='week'),
+                        get_ema(ticker=ticker, window=10, timespan='week'),
+                        get_ema(ticker=ticker, window=20, timespan='week'),
+                        get_macd(ticker=ticker, timespan='week')
+                    )
+
+        calculate_keltner_channels(weekly_df, window=26)
+        calculate_atr(weekly_df)
+        calculate_impulse(weekly_df)
+        timestamp_to_date(weekly_df)
+
+        insert_data(
+            df=weekly_df,
+            table_name='weekly',
+            db_filepath=DB_FILEPATH
+        )
+
 
 
 if __name__ == '__main__':

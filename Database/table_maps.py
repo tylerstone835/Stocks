fc771513139258643b08e9a3b1f114ef44467f63
tylerstone835 <144369580+tylerstone835@users.noreply.github.com>
2025-08-04 18:53:36 -0600

    updated table map to only focus on SQL table structure.

--- Database/table_maps.py
+++ Database/table_maps.py
@@ -1,108 +1,70 @@
-from dataframe import timestamp_to_date
-from polygon_api import *
-
-
 daily_table_map = {
     'date': {
         'dtype': 'DATE',
-        'constraint': 'PRIMARY KEY',
-        'callable': timestamp_to_date,
-        'params': None
+        'constraint': 'PRIMARY KEY'
     },
     'symbol': {
         'dtype': 'VARCHAR(10)',
-        'constraint': 'PRIMARY KEY',
-        'callable': None,
-        'params': None
+        'constraint': 'PRIMARY KEY'
     },
     'open': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': 'NOT NULL',
-        'callable': get_price_action,
-        'params': {'multiplier': 1, 'timespan': 'day'}
+        'constraint': 'NOT NULL'
     },
     'high': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': 'NOT NULL',
-        'callable': None,
-        'params': None
+        'constraint': 'NOT NULL'
     },
     'low': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': 'NOT NULL',
-        'callable': None,
-        'params': None
+        'constraint': 'NOT NULL'
     },
     'close': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': 'NOT NULL',
-        'callable': None,
-        'params': None
+        'constraint': 'NOT NULL'
     },
     'volume': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': 'NOT NULL',
-        'callable': None,
-        'params': None
+        'constraint': 'NOT NULL'
     },
     'sma_50': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': '',
-        'callable': get_sma,
-        'params': {'timespan': 'day', 'window': 50}
+        'constraint': ''
     },
     'ema_5': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': '',
-        'callable': get_ema,
-        'params': {'timespan': 'day', 'window': 5}
+        'constraint': ''
     },
     'ema_10': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': '',
-        'callable': get_ema,
-        'params': {'timespan': 'day', 'window': 10}
+        'constraint': ''
     },
     'ema_20': {
         'dtype': 'NUMBER(20, 3)',
-        'constraint': '',
-        'callable': get_ema,
-        'params': {'timespan': 'day', 'window': 20}
+        'constraint': ''
     },
     'macd_histogram': {
         'dtype': 'NUMBER(20, 7)',
-        'constraint': '',
-        'callable': get_macd,
-        'params': {'timespan': 'day'}
+        'constraint': ''
     },
     'deviation': {
         'dtype': 'NUMBER(20, 4)',
-        'constraint': '',
-        'callable': None,
-        'params': None
+        'constraint': ''
     },
     'upper_channel': {
         'dtype': 'NUMBER(20, 2)',
-        'constraint': '',
-        'callable': None,
-        'params': None
+        'constraint': ''
     },
     'lower_channel': {
         'dtype': 'NUMBER(20, 2)',
-        'constraint': '',
-        'callable': None,
-        'params': None
+        'constraint': ''
     },
     'atr': {
         'dtype': 'NUMBER(20, 2)',
-        'constraint': '',
-        'callable': None,
-        'params': None
+        'constraint': ''
     },
     'impulse': {
         'dtype': 'VARCHAR(5)',
-        'constraint': '',
-        'callable': None,
-        'params': None
+        'constraint': ''
     }
 }

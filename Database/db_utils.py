1e7c12629bb2e92032a1d47755e148c4517eceaf
tylerstone835 <144369580+tylerstone835@users.noreply.github.com>
2025-09-01 23:51:54 -0600

    get_missing_weeks() added

--- Database/db_utils.py
+++ Database/db_utils.py
@@ -9,7 +9,7 @@ import sqlite3
 DB_FILEPATH = os.environ.get('STOCK_DATABASE')
 
 """
-Supressing the futurewarning regarding the future behavior of pd.concat.
+Suppressing the futurewarning regarding the future behavior of pd.concat.
 pd.DataFrame objects with blank/null values is expected and allowed in this
 use case.
 """
@@ -80,7 +80,7 @@ def insert_data(
 
 
 def get_missing_days(
-    table_name: str
+    table_name: str = 'daily'
 ) -> list[date]:
     """
     Identifies how many days of data is missing from database table.
@@ -100,10 +100,39 @@ def get_missing_days(
     last_date = datetime.strptime(response.fetchone()[0], '%Y-%m-%d').date()
     today = date.today()
 
-    date_array = []
-    for i in range((today - last_date).days):
-        new_date = last_date + timedelta(days=i + 1)
-        if new_date.weekday() < 5:
-            date_array.append(new_date)
+    # Append missing date if not Saturday/Sunday
+    date_array = [last_date + timedelta(days=i + 1) for i in range((today - last_date).days)
+                  if (last_date + timedelta(days=i + 1)).weekday() < 5]
 
     return date_array
+
+
+def get_missing_weeks(
+    table_name: str = 'weekly'
+) -> list[date]:
+    """
+    Identifies how many weeks of data is missing from database table.
+    Only returns mondays, representing the entire stock week. Always
+    includes the latest Monday in weekly table, as it's impossible to know
+    when in the week it was last updated.
+
+    :param table_name: Name of target SQL table.
+    :return: array of datetime.date that are missing from DB.
+    """
+
+    with sqlite3.connect(DB_FILEPATH) as conn:
+        cursor = conn.cursor()
+        response = cursor.execute(f'SELECT MAX(date) FROM {table_name}')
+
+    if not response:
+        return []
+
+    last_date = datetime.strptime(response.fetchone()[0], '%Y-%m-%d').date()
+    today = date.today()
+
+    # Append date if Monday
+    date_array = [last_date + timedelta(days=i) for i in range((today - last_date).days + 1)
+                  if (last_date + timedelta(days=i)).weekday() == 0]
+
+    if date_array:
+        return date_array

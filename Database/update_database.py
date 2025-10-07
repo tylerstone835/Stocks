6b1efc1c14fc157bf15de9be47cb341efc8d3f52
tylerstone835 <144369580+tylerstone835@users.noreply.github.com>
2025-10-07 00:58:39 -0600

    Base data update pipeline complete.

--- Database/update_database.py
+++ Database/update_database.py
@@ -1,17 +1,28 @@
 from procedures import *
-
+from datetime import datetime
 
 def main():
+
+    # ___________ update daily table ___________
     update_daily_price_action()
     update_sma(window=50, table='daily')
     update_ema(window=5, table='daily')
     update_ema(window=10, table='daily')
     update_ema(window=20, table='daily')
+    update_macd(table='daily')
+    update_keltner(table='daily')
+    update_atr(table='daily')
+    update_impulse(table='daily')
 
+    # ___________ update weekly table ___________
     update_weekly_price_action()
     update_ema(window=5, table='weekly')
     update_ema(window=10, table='weekly')
     update_ema(window=20, table='weekly')
+    update_macd(table='weekly')
+    update_keltner(table='weekly', channel_window=26)
+    update_atr(table='weekly')
+    update_impulse(table='weekly')
 
 
 if __name__ == '__main__':

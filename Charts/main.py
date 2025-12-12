55808fe557d7bbb41a0905f36a33c5e3224ffc57
tst <tylerstone835@gmail.com>
2025-12-11 22:57:51 -0700

    Finished image creation pipeline

--- Charts/main.py
+++ Charts/main.py
@@ -4,41 +4,61 @@ import sqlite3
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from chart_utils import plot_macd, plot_ohlc
+from chart_utils import plot_macd, plot_ohlc, plot_volume
 from queries import *
 
+# Remove existing daily charts
+image_dir = '/home/tst/python/Stocks/Charts/Daily'
+
+for image in os.listdir(image_dir):
+    os.remove('/'.join([image_dir, image]))
+
+
 with sqlite3.connect(os.environ.get('STOCK_DATABASE')) as con:
-    stock_data_df = pd.read_sql_query(
-        con=con,
-        sql=price_action_query('LEG')
-    )
 
     bom_df = pd.read_sql_query(
         con=con,
         sql=beginning_of_month_query()
     )
 
-fig, ax = plt.subplots(
-    nrows=2,
-    ncols=1,
-    sharex=True,
-    height_ratios=[.8, .2],
-    figsize=(20, 10)
-)
+    symbols_df = pd.read_sql_query(
+        con=con,
+        sql=qualifying_symbols_query()
+    )
 
-plot_macd(
-    axes=ax[1],
-    df=stock_data_df,
-    xticks=bom_df['BOM']
-)
+    for symbol in symbols_df['symbol']:
 
-plot_ohlc(
-    axes=ax[0],
-    df=stock_data_df,
-    xticks=bom_df['BOM']
-)
+        stock_data_df = pd.read_sql_query(
+            con=con,
+            sql=price_action_query(symbol)
+        )
 
-plt.tight_layout()
-plt.show()
-#plt.savefig(f'Daily\\LEG.png')
-#plt.close()
+        fig, ax = plt.subplots(
+            nrows=3,
+            ncols=1,
+            sharex=True,
+            height_ratios=[.73, .07, .2],
+            figsize=(20, 10)
+        )
+
+        plot_macd(
+            axes=ax[2],
+            df=stock_data_df,
+            xticks=bom_df['BOM']
+        )
+
+        plot_volume(
+            axes=ax[1],
+            df=stock_data_df,
+            xticks=bom_df['BOM']
+        )
+
+        plot_ohlc(
+            axes=ax[0],
+            df=stock_data_df,
+            xticks=bom_df['BOM']
+        )
+
+        plt.tight_layout()
+        plt.savefig(f'/home/tst/python/Stocks/Charts/Daily/{symbol}.png')
+        plt.close()
